8ee538c1649bac0034dd565adadbb1bd92727368
khechinimed <khechinibakr20@gmail.com>
2023-11-17 11:06:39 +0100

    adding ubuntu_ami for private instances

--- datasources.tf
+++ datasources.tf
@@ -1,11 +1,33 @@
 data "aws_ami" "ami" {
 
     most_recent = true
+    owners = ["amazon"]
 
     filter {
         name   = "name"
         values = ["amzn-ami-vpc-nat-2018.03.0.2021*"]
     }
 
-    owners = ["amazon"]
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+
+}
+
+// create ami with ubuntu 20.04
+data "aws_ami" "ubuntu_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
 }
