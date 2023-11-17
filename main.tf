59998d117cfc55ecb72d00e04516abfa9cca4b4c
khechinimed <khechinibakr20@gmail.com>
2023-11-17 11:09:43 +0100

    aws private and public instances

--- main.tf
+++ main.tf
@@ -14,12 +14,42 @@ locals {
     )
 }
 
+resource "aws_eip" "eip_static" {
+  for_each = var.availability_zones
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  //find all instance ids in the private
+  for_each      = var.availability_zones
+  instance_id   = aws_instance.public_instance[each.key].id
+  allocation_id = aws_eip.eip_static[each.key].id
+}
+
 resource "aws_key_pair" "deployer-key" {
   key_name   = "khechini-key"
   public_key = "${file(var.aws_keypair_file)}"
 }
 
-resource "aws_instance" "nat" {
+resource "aws_instance" "private_instance" {
+  for_each = var.availability_zones
+
+  ami           = data.aws_ami.ubuntu_ami.id
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.private[each.key].id
+  key_name = aws_key_pair.deployer-key.key_name
+  vpc_security_group_ids = [aws_security_group.name.id]
+
+  source_dest_check = false
+
+  tags = {
+    Name = "${var.aws_region}${each.key}-ec2-private"
+    Environment = var.environment
+    Owner = "Khechini Mohamed"
+  }
+}
+
+resource "aws_instance" "public_instance" {
   for_each = var.availability_zones
 
   ami           = data.aws_ami.ami.id
@@ -29,8 +59,10 @@ resource "aws_instance" "nat" {
   key_name = aws_key_pair.deployer-key.key_name
   vpc_security_group_ids = [aws_security_group.name.id]
 
+  source_dest_check = false
+
   tags = {
-    Name = "${var.aws_region}${each.key}-ec2"
+    Name = "${var.aws_region}${each.key}-ec2-public"
     Environment = var.environment
     Owner = "Khechini Mohamed"
   }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 # outputs.tf
 output "nat_pub_ips" {
-  value = [for ec2 in aws_instance.nat : ec2.public_ip]
+  value = [for ec2 in aws_instance.public_instance : ec2.public_ip]
 }
